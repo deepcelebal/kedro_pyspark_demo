216d1c701d947bad2655a39b1e6c2c41acff80a2
Deep Contractor <deep.contractor88@yahoo.com>
2022-03-07 17:15:57 +0530

    inf changes

--- src/pysprk_kedro_demo/pipelines/inference/nodes.py
+++ src/pysprk_kedro_demo/pipelines/inference/nodes.py
@@ -2,10 +2,8 @@
 import logging
 import mlflow
 from mlflow import spark
-from mlflow import sklearn
 import datetime as dt
 from typing import Any, Dict
-# from pyspark.ml.classification import RandomForestClassifier
 from pyspark.ml.classification import GBTClassifier
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.sql import DataFrame
@@ -17,7 +15,7 @@ def get_inference(testing_data: DataFrame) -> DataFrame:
     logged_model = 'runs:/873d89ca676246c691c1a3c0ce901104/model_rf'
 
     # Load model as a Spark UDF. Override result_type if the model does not return double values.
-    loaded_model = mlflow.pyfunc.spark_udf(spark, model_uri=logged_model, result_type='double')
+    loaded_model = mlflow.spark.load_model(logged_model)
     predictions = loaded_model.transform(testing_data)
 
     return predictions
