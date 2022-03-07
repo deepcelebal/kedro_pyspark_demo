a278683436f1a8d796e063201b96aa355f29394b
Deep <100366237+deepcelebal@users.noreply.github.com>
2022-03-07 12:07:10 +0530

    Update nodes.py

--- src/pysprk_kedro_demo/pipelines/data_science/nodes.py
+++ src/pysprk_kedro_demo/pipelines/data_science/nodes.py
@@ -5,6 +5,8 @@ such as:
 * Evaluate the model based on its prediction
 """
 import logging
+import mlflow
+from mlflow import sklearn
 import datetime as dt
 from typing import Any, Dict
 from pyspark.ml.classification import RandomForestClassifier
@@ -18,11 +20,18 @@ def train_model(training_data: DataFrame, parameters: Dict[str, Any]) -> RandomF
     and passed into this node via the `parameters` argument.
     For more information about random forest classifier with spark, please visit:
     https://spark.apache.org/docs/latest/ml-classification-regression.html#random-forest-classifier
-    """ 
+    """
+    experiment_id = mlflow.create_experiment("Agent_Routing_Pyspark" + str(dt.datetime.now()))
+    experiment = mlflow.get_experiment(experiment_id)
+    
+    with mlflow.start_run(experiment_id=experiment.experiment_id):
 
-    classifier = RandomForestClassifier(featuresCol = 'features', labelCol = 'Converted')
-    rfModel = classifier.fit(training_data)
-    print('*******************Training Finished*******************')
+        classifier = RandomForestClassifier(featuresCol = 'features', labelCol = 'Converted')
+        rfModel = classifier.fit(training_data)
+        sklearn.log_model(
+                    sk_model=rfModel, artifact_path="model"
+                )
+        print('*******************Training Finished*******************')
 
 
 # def predict(model: RandomForestClassifier, testing_data: DataFrame) -> DataFrame:
